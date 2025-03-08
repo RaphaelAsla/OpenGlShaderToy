82af913ea8e9dac575d7c287876753e47273affa
Reavey <65507003+RaphaelAsla@users.noreply.github.com>
2025-03-08 08:00:49 +0200

    some redurant stuff

--- shaders/wormy/wormy.frag.glsl
+++ shaders/wormy/wormy.frag.glsl
@@ -37,8 +37,8 @@ float sdfMap(vec3 p) {
     b1.x = mod(b1.x, 0.3) - 0.15;
     b1.y = mod(b1.y, 1.0) - 0.5;
     b1.z = mod(b1.z, 1.5) - 0.75;
-    b1.yz *= rot2D(sin(p.x * 6.28 + time));
-    b1.xy *= rot2D(-time * 2.0);
+    b1.yz *= rot2D(sin(p.x * 6.28));
+    b1.xy *= rot2D(time * 2.0);
     float w1 = sdfBox(b1, vec3(0.1));
 
     vec3 b2 = p;
@@ -49,15 +49,15 @@ float sdfMap(vec3 p) {
     b2.y = mod(b2.y, 0.3) - 0.15;
     b2.x = mod(b2.x, 1.0) - 0.5;
     b2.z = mod(b2.z, 1.5) - 0.75;
-    b2.xz *= rot2D(sin(p.y * 6.28 + time));
-    b2.xy *= rot2D(-time * 2.0);
+    b2.xz *= rot2D(sin(p.y * 6.28));
+    b2.xy *= rot2D(time * 3.14);
     float w2 = sdfBox(b2, vec3(0.1));
 
     vec3 b3 = p;
     b3.xy = fract(b3.xy) - 0.5;
     b3.z = mod(b3.z, 0.2) - 0.1;
-    b3.xy *= rot2D(sin(p.z * 6.28 + time));
-    b3.xy *= rot2D(time * 2.0);
+    b3.xy *= rot2D(sin(p.z * 6.28));
+    b3.xy *= rot2D(time * 3.14);
     float c = sdfBox(b3, vec3(0.1));
 
     return smin(hbox, min(min(w1, w2), c), 0.55);
@@ -79,20 +79,15 @@ void main() {
 
     vec3 ro = vec3(0.0, 0.0, 0.0);
     vec3 rd = normalize(vec3(uv, 1.0));
-
     rd.yz *= rot2D((-2.0 + sin(time * 0.5)) * 0.5);
     rd.xy *= rot2D(time * 0.2);
 
     float t = 0.0;
-
     int i = 0;
     for (i = 0; i < 128; i++) {
         vec3 p = ro + rd * t;
-
         float d = sdfMap(p);
-
         t += d / 2.0;
-
         if (d < 0.01 || t > 100.0) {
             break;
         }
